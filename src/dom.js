9a4591ac5aae4c5054eb2326c06bea09fb3cf1ef
“noLakes” <51928001+noLakes@users.noreply.github.com>
2021-01-19 20:28:28 -0500

    added initial dom elements

---
+++ src/dom.js
@@ -0,0 +1,23 @@
+
+const elements = (function() {
+  
+  const main = document.querySelector('.main-container');
+  const nav = document.querySelector('.nav-container');
+  const static_links = document.querySelector('.static-links');
+  const field_links = document.querySelector('.field-links');
+  const content = document.querySelector('.content-container');
+  const footer = document.querySelector('.footer-container');
+
+  return {
+
+  }
+})()
+
+const render = (function() {
+  
+  return {
+
+  }
+})()
+
+export { elements, render }
