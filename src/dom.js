cfdea23d49cdcb4e0f20941c2e582ca1253cf74d
“noLakes” <51928001+noLakes@users.noreply.github.com>
2021-01-19 21:07:09 -0500

    added early versions of element, render and controller modules

--- src/dom.js
+++ src/dom.js
@@ -1,23 +1,48 @@
 
+// a basic library for global elements and reusable functions
 const elements = (function() {
-  
-  const main = document.querySelector('.main-container');
-  const nav = document.querySelector('.nav-container');
-  const static_links = document.querySelector('.static-links');
-  const field_links = document.querySelector('.field-links');
-  const content = document.querySelector('.content-container');
-  const footer = document.querySelector('.footer-container');
-
   return {
+    main : document.querySelector('.main-container'),
+    nav : document.querySelector('.nav-container'),
+    static_links : document.querySelector('.static-links'),
+    field_links : document.querySelector('.field-links'),
+    content : document.querySelector('.content-container'),
+    footer : document.querySelector('.footer-container'),
+
+    basic(type, className=undefined, id=undefined) {
+      const div = document.createElement(type);
+      if (className) div.classList.add(className);
+      if (id) div.id = id;
+      return div;
+    },
 
   }
 })()
 
+// a way to render objects into appropriate dom elements
 const render = (function() {
-  
+  return {
+
+    leaf(leaf) {
+
+    },
+
+    head(head) {
+      const container = elements.basic('div', 'head');
+    },
+
+    field() {
+
+    },
+
+  }
+})()
+
+// a logic controller for the different ways you might want to render/show the content
+const controller = (function() {
   return {
 
   }
 })()
 
-export { elements, render }
+export { elements, render, controller };
